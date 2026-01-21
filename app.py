288d0257cae8194c1e144a3f1901ff877b0d4c98
gselderz <gselderz@gmail.com>
2026-01-21 06:38:10 +0000

    Final changes

--- app.py
+++ app.py
@@ -7,13 +7,5 @@ app = Flask(__name__)
 def index():
     return(render_template("index.html"))
 
-@app.route("/prediction",methods=["GET","POST"])
-def prediction():
-    q = float(request.form.get("q"))
-    model = joblib.load('dbs.jl')
-    pred = model.predict([[q]])
-    r = pred[0][0]
-    return(render_template("prediction.html", r=r))
-
 if __name__=="__main__":
     app.run()
